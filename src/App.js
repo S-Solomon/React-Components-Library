58ab79e218205e972d4ce4bfb0b560b07fe11747
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-09-10 17:48:55 +0100

    del

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import Toolbar from './components/Toolbar/Toolbar';
-import SideDrawer from './components/SideDrawer/SideDrawer'
-import Backdrop from './components/Backdrop/Backdrop'
+
 
 function App() {
-  // const [sideDrawerOpen, setSideDrawerOpen] = useState(false)
-
-  // const drawerToggler = () => {
-  //   setSideDrawerOpen(true)
-  // }
-
-  // let sideDrawer;
-  // let backdrop;
-
-  // if(sideDrawerOpen) {
-  //   sideDrawer = <SideDrawer />;
-  //   backdrop = <Backdrop />
-  // }
 
   return (
-    <div style={{height: '100%'}}>
-      <Toolbar />
-      {/* {sideDrawer} */}
-      {/* {backdrop} */}
-      <SideDrawer />
-      <Backdrop />
-      <main style={{marginTop: '80px'}}>
-        <p>This is the page content</p>
-      </main>
-    </div>
+    <>
+      <h1>welcome to react</h1>
+    </>
   );
 }
 
