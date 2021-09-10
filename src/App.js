100c2e912df416617fe14fe79db5f01b0abddd7b
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-09-10 17:47:13 +0100

    del

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer'
+import Backdrop from './components/Backdrop/Backdrop'
 
 function App() {
+  // const [sideDrawerOpen, setSideDrawerOpen] = useState(false)
+
+  // const drawerToggler = () => {
+  //   setSideDrawerOpen(true)
+  // }
+
+  // let sideDrawer;
+  // let backdrop;
+
+  // if(sideDrawerOpen) {
+  //   sideDrawer = <SideDrawer />;
+  //   backdrop = <Backdrop />
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: '100%'}}>
+      <Toolbar />
+      {/* {sideDrawer} */}
+      {/* {backdrop} */}
+      <SideDrawer />
+      <Backdrop />
+      <main style={{marginTop: '80px'}}>
+        <p>This is the page content</p>
+      </main>
     </div>
   );
 }
